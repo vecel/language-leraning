d00166b08b44014cdb073b7f981698968c1b07a7
vecel <mateusz.karandys@gmail.com>
2025-01-18 15:40:13 +0100

    Add express-async-errors

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,26 @@
+require('express-async-errors')
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
 const connectDB = require('./db')
 
 const usersRouter = require('./controllers/users')
-
 const app = express()
 
 connectDB()
 
-app.use(express.json())
-
 const errorHandler = (error, request, response, next) => {
-  logger.warn(error.message)
   if (error.name === 'CastError')
     response.status(400).send({ error: 'Malformatted id' })
+  if (error.name === 'ValidationError')
+    response.status(400).send({ error: error.message })
+  if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error collection'))
+    response.status(400).send({ error: 'Expected `username` to be unique' })
   next(error)
 }
+
+app.use(express.json())
 
 app.use('/api/users', usersRouter)
 
