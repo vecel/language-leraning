0a68811bfd50bd652cee42ca229f699cd99116fe
vecel <mateusz.karandys@gmail.com>
2025-01-18 17:22:52 +0100

    Refactor tests

--- backend/tests/user_api.test.js
+++ backend/tests/user_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert/strict')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -21,74 +21,80 @@ const mockUsers = [
     }
 ]
 
-beforeEach(async () => {
-    await User.deleteMany({})
-    for (user of mockUsers) {
-        const userObject = new User(user)
-        await userObject.save()
-    }
-})
+describe('There are some users saved in database initially', () => {
 
-test('Users are returned as json', async () => {
-    await api
-        .get('/api/users')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-
-test('There are exactly two users', async () => {
-    const response = await api.get('/api/users')
-    assert.strictEqual(response.body.length, 2)
-})
-
-test('User has username and email', async () => {
-    const response = await api.get('/api/users')
-    const user = response.body[0]
-    assert(Object.hasOwn(user, 'username') && Object.hasOwn(user, 'email'))
-})
-
-test('New user can be added', async () => {
-    const newUser = {
-        username: 'Andrzej',
-        email: 'andrzej@gmail.com',
-        password: 'strong_password'
-    }
+    beforeEach(async () => {
+        await User.deleteMany({})
+        for (user of mockUsers) {
+            const userObject = new User(user)
+            await userObject.save()
+        }
+    })
     
-    await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(201)
+    test('Users are returned as json', async () => {
+        await api
+            .get('/api/users')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    
+    test('There are exactly two users', async () => {
+        const response = await api.get('/api/users')
+        assert.strictEqual(response.body.length, 2)
+    })
+    
+    test('User has username and email', async () => {
+        const response = await api.get('/api/users')
+        const user = response.body[0]
+        assert(Object.hasOwn(user, 'username') && Object.hasOwn(user, 'email'))
+    })
+    
+    describe('Adding new user', () => {
 
-    const response = await api.get('/api/users')
-    assert.strictEqual(response.body.length, mockUsers.length + 1)
-})
-
-test('Cannot add user with non-unique username', async () => {
-    const newUser = {
-        username: 'Mateusz',
-        email: 'mail@gmail.com',
-        password: 'strong_password'
-    }
-
-    await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(400)
-})
-
-test('Cannot add user with non-unique email', async () => {
-    const newUser = {
-        username: 'Andrzej',
-        email: 'mateusz@gmail.com',
-        password: 'strong_password'
-    }
-
-    await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(400)
-})
-
-after(async () => {
-    await mongoose.connection.close()
+        test('Succeeds with valid data', async () => {
+            const newUser = {
+                username: 'Andrzej',
+                email: 'andrzej@gmail.com',
+                password: 'strong_password'
+            }
+            
+            await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(201)
+        
+            const response = await api.get('/api/users')
+            assert.strictEqual(response.body.length, mockUsers.length + 1)
+        })
+        
+        test('Fails with status 400, when username is not unique', async () => {
+            const newUser = {
+                username: 'Mateusz',
+                email: 'mail@gmail.com',
+                password: 'strong_password'
+            }
+        
+            await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+        })
+        
+        test('Fails with status 400, when email is not unique', async () => {
+            const newUser = {
+                username: 'Andrzej',
+                email: 'mateusz@gmail.com',
+                password: 'strong_password'
+            }
+        
+            await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+        })
+        
+        after(async () => {
+            await mongoose.connection.close()
+        })
+    })
 })
