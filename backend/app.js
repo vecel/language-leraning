afccdcec5612d1ecc06179c4eab2ce3d6526c45a
vecel <mateusz.karandys@gmail.com>
2025-01-24 19:26:28 +0100

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 require('express-async-errors')
 
 const express = require('express')
+const cors = require('cors')
+
 const connectDB = require('./db')
 const { UserCreationError, AuthorizationError } = require('./utils/errors')
 
@@ -26,6 +28,7 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
